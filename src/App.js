f3c94e7341d20f674eee8c45d11227c91fbdf4d9
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-04-10 01:08:02 +0600

    protected route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './component/Header/Header';
-import { Route, Routes } from 'react-router-dom';
-import Home from './component/Home/Home';
-import Login from './component/Login/Login';
-import Registration from './component/Registration/Registration';
-import Product from './component/Product/Product';
-import Orders from './component/Orders/Orders';
-import About from './component/About/About';
-import RequireAuth from './component/RequireAuth/RequireAuth';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./component/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./component/Home/Home";
+import Login from "./component/Login/Login";
+import Registration from "./component/Registration/Registration";
+import Product from "./component/Product/Product";
+import Orders from "./component/Orders/Orders";
+import About from "./component/About/About";
+import RequireAuth from "./component/RequireAuth/RequireAuth";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/products' element={<Product></Product>}></Route>
-        <Route path='/orders' element={<RequireAuth><Orders></Orders></RequireAuth>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/registration' element={<Registration></Registration>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/products" element={<Product></Product>}></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route
+          path="/registration"
+          element={<Registration></Registration>}
+        ></Route>
       </Routes>
     </div>
   );
--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,32 +1,47 @@
-import { getAuth } from 'firebase/auth';
-import React from 'react';
-import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
-import app from '../../firebase.init';
-import {FcGoogle} from 'react-icons/fc';
-
+import { getAuth } from "firebase/auth";
+import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import app from "../../firebase.init";
+import { FcGoogle } from "react-icons/fc";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const auth = getAuth(app);
 
 const Login = () => {
-    const [singInWithGoogle] = useSignInWithGoogle(auth);
+  const [singInWithGoogle] = useSignInWithGoogle(auth);
 
-    const handleSubmit = event => {
-        event.preventDefault();
-    }
-    return (
-        <div>
-            <h2>Please Login</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="" id="" placeholder='Your Email'/>
-                <br />
-                <input type="password" name="" id="" placeholder='Password'/>
-                <br />
-                <button type="submit">Login</button>
-                <br />
-                <button style={{marginTop:'10px', borderRadius:'10px', cursor:'pointer'}} onClick={() => singInWithGoogle()}><FcGoogle style={{fontSize: '30px'}}></FcGoogle></button>
-            </form>
-        </div>
-    );
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location?.state?.from?.pathname || "/";
+
+  const handleSignIn = () => {
+    singInWithGoogle().then(() => {
+      navigate(from, { replace: true });
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <h2>Please Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="" id="" placeholder="Your Email" />
+        <br />
+        <input type="password" name="" id="" placeholder="Password" />
+        <br />
+        <button type="submit">Login</button>
+        <br />
+        <button
+          style={{ marginTop: "10px", borderRadius: "10px", cursor: "pointer" }}
+          onClick={() => handleSignIn()}
+        >
+          <FcGoogle style={{ fontSize: "30px" }}></FcGoogle>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
